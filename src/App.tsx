89ad5147a1ff39299fcf8a9208a42db2b4de26e9
Emil Lystimaki <emil@circularway.com>
2023-12-13 18:03:31 +0000

    style: 💄 backdrop blur text input

--- src/App.tsx
+++ src/App.tsx
@@ -51,11 +51,12 @@ type Match = {
   matched: Date;
 };
 
-function MatchTextBubble(props: { message: Message }) {
+function MatchTextBubble(props: { message: Message, isLatest: boolean }) {
   const { message } = props.message;
+  const spacingFromBottom = props.isLatest ? "mb-[21%]" : "mb-0";
 
   return (
-    <li className="flex w-full items-center justify-start">
+    <li className={`flex w-full items-center justify-start ${spacingFromBottom}`}>
       <div className="leading-1.5 flex w-full max-w-[320px] flex-col gap-2.5 rounded-r-2xl rounded-tl-2xl  bg-[#e7e9f0] p-3 text-slate-600">
         <p>{message}</p>
       </div>
@@ -63,11 +64,12 @@ function MatchTextBubble(props: { message: Message }) {
   );
 }
 
-function SenderTextBubble(props: { message: Message }) {
+function SenderTextBubble(props: { message: Message, isLatest: boolean }) {
   const { message } = props.message;
+  const spacingFromBottom = props.isLatest ? "mb-[21%]" : "mb-0";
 
   return (
-    <li className="flex w-full items-center justify-end">
+    <li className={`flex w-full items-center justify-end ${spacingFromBottom}`}>
       <div className="leading-1.5 flex w-full max-w-[320px] flex-col gap-2.5 rounded-l-2xl rounded-tr-2xl  bg-[#dd5170] p-3 text-neutral-50">
         <p>{message}</p>
       </div>
@@ -87,11 +89,11 @@ function MessagesList(props: {
   return (
     <ul className="flex w-full flex-col gap-4 px-4 py-2">
       <MatchText matchedOn={props.match.matched} />
-      {props.messages.map((msg) =>
+      {props.messages.map((msg, i) =>
         msg.user.id !== props.user.id ? (
-          <MatchTextBubble message={msg} key={msg.id} />
+          <MatchTextBubble message={msg} key={msg.id} isLatest={i === props.messages.length - 1} />
         ) : (
-          <SenderTextBubble message={msg} key={msg.id} />
+          <SenderTextBubble message={msg} key={msg.id} isLatest={i === props.messages.length - 1} />
         ),
       )}
     </ul>
@@ -129,7 +131,6 @@ function App() {
         user: { id: 1 },
         id: 3,
       },
-      /**
       {
         message: "Hey! Did you also go to Oxford?",
         sentAt: new Date(),
@@ -185,21 +186,20 @@ function App() {
         user: { id: 1 },
         id: 12,
       },
-      */
     ],
   };
 
   const tabClass = (tab: ScreenView) => {
     if (tab === screenView) {
-      return "border-b-4 border-[#dd5170] font-semibold  text-[#dd5170] w-full";
+      return "border-b-[3px] border-[#dd5170] font-semibold  text-[#dd5170] w-full";
     } else {
-      return "font-medium text-slate-400 w-full";
+      return "font-medium text-slate-400 w-full border-b-[3px]";
     }
   };
 
   return (
     <main className="flex h-screen flex-col items-center justify-center bg-neutral-50">
-      <nav className="bottom-shadow h-[12%] w-full bg-neutral-50 drop-shadow-sm">
+      <nav className="bottom-shadow h-fit w-full bg-neutral-50 ">
         <ul className="flex items-center justify-between px-2 py-3">
           <li>
             <a className="-ml-1 flex w-fit items-center justify-center">
@@ -227,7 +227,7 @@ function App() {
               className={tabClass(ScreenView.Chat)}
               onClick={() => setScreenView(ScreenView.Chat)}
             >
-              <p className="py-2">Chat</p>
+              <p className="py-1">Chat</p>
             </button>
           </li>
           <li className="w-full text-center">
@@ -235,31 +235,34 @@ function App() {
               className={tabClass(ScreenView.Profile)}
               onClick={() => setScreenView(ScreenView.Profile)}
             >
-              <p className="py-2">Profile</p>
+              <p className="py-1">Profile</p>
             </button>
           </li>
         </ul>
       </nav>
       {screenView === ScreenView.Chat ? (
-        <div className="flex h-[88%] w-full flex-col items-center justify-center bg-neutral-50">
-          <div className="flex h-[89%] w-full flex-col justify-end gap-4 overflow-auto shadow-inner">
-            <MessagesList
-              messages={data.messages}
-              match={data.match}
-              user={data.user}
-            />
-          </div>
-          <div className="flex h-[10%] w-full items-center justify-center border-t-[1.8px] border-slate-200 px-4 py-4">
-            <form className="flex w-full items-center justify-center">
-              <input
-                placeholder={`Message ${data.match.name}`}
-                className="w-full rounded-full border-[1.5px] border-slate-300 px-3 py-2"
+        <div className="h-full w-full flex flex-col items-center justify-center relative ">
+          <div className="absolute top-0 flex h-full w-full flex-col items-center justify-end bg-neutral-50">
+            <div className="flex h-full w-full flex-col  gap-4 overflow-auto">
+              <MessagesList
+                messages={data.messages}
+                match={data.match}
+                user={data.user}
               />
-            </form>
+            </div>
+            <div className="absolute bottom-0 flex h-[10%] bg-white/30 w-full items-center justify-center  border-t-[1.8px] border-slate-200 px-4 py-4 backdrop-blur-xl">
+              <form className="flex w-full items-center justify-center">
+                <input
+                  placeholder={`Message ${data.match.name}`}
+                  className="w-full rounded-full border-[1.5px] border-slate-300 px-3 py-2"
+                />
+              </form>
+
+            </div>
           </div>
         </div>
       ) : (
-        <div className="h-full">
+        <div className="h-[88%]">
           <p>Profile</p>
         </div>
       )}
